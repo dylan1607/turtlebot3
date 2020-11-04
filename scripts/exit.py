54609fa267ffcb35d13bf4e116c767c0edc68424
Workspace <nguyen.tran@spclt.com.vn>
2020-11-04 13:23:23 +0700

    change_file

--- scripts/exit.py
+++ scripts/exit.py
@@ -4,6 +4,5 @@ import subprocess
 import os
 
 if __name__ == '__main__':
+    subprocess.Popen('killall -9 rosmaster roscore python', shell=True)
     subprocess.Popen('pkill -9 python', shell=True)
-    subprocess.Popen('killall -9 rosmaster python', shell=True)
-    
